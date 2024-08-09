252f42005130987de2399e321586d95aeea584b7
sam <sam@jennifersoft.com>
2024-08-09 12:48:08 +0900

    index.js 파일 정리

--- index.js
+++ index.js
@@ -3,22 +3,11 @@ const swaggerUi = require('swagger-ui-express');
 const jsYaml = require('js-yaml');
 const fs = require('fs');
 const path = require('path');
-
 const app = express();
-
-// Define the options for the OpenAPI middleware
-const options = {
-    routing: {
-        controllers: path.join(__dirname, './controllers')
-    },
-};
 
 // Load the OpenAPI document
 const openApiDocumentPath = path.join(__dirname, './api/openapi.yaml');
 const openApiDocument = jsYaml.load(fs.readFileSync(openApiDocumentPath, 'utf8'));
-
-const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css"
-
 
 // Explicitly serve the favicon
 app.get('/favicon.ico', (req, res) => {
@@ -28,8 +17,10 @@ app.get('/favicon.ico', (req, res) => {
 // Serve Swagger UI with customization options
 app.use('/', swaggerUi.serve, swaggerUi.setup(openApiDocument, {
     customSiteTitle: 'Jennifer5 Open API',
+    // 과거 버전의 swagger css 에 맞지 않는 부분 일부를 수정해서 사용함
     customCss: '.swagger-ui .topbar { display: none } .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
-    customCssUrl: CSS_URL,
+    // swagger 를 vercel 에 배포할 때 cdn 관련 이슈가 있는듯. 외부 css 를 참조하게 해서 우회한다.
+    customCssUrl: "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css",
     customfavIcon: 'favicon.ico',
 }));
 
